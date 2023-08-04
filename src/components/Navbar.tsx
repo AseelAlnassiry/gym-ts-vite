be8112d285dc9c56b5c99fcfd7abc465cd26d0e2
aseelalnassiry <aseelalnassiry@gmail.com>
2023-08-03 21:08:31 -0700

    project init

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/20/solid";
+import Logo from "@/assets/Logo.png";
+
+type Props = {};
+const Navbar = (props: Props) => {
+  return (
+    <nav>
+      <div className="flex-between fixed top-0 z-30 w-full py-6">
+        
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
